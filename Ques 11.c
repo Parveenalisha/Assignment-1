59fb2ee3ed127229d7ad8e3f80758924c0692cb8
Parveenalisha <133557716+Parveenalisha@users.noreply.github.com>
2023-05-14 11:15:04 +0530

    Create Ques 11.c

---
+++ Ques 11.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+int main() {
+
+    float operand1, operand2;
+
+    char operator;
+
+    printf("Enter first operand: ");
+
+    scanf("%f", &operand1);
+
+    printf("Enter operator (+, -, *, /): ");
+
+    scanf(" %c", &operator);
+
+    printf("Enter second operand: ");
+
+    scanf("%f", &operand2);
+
+    float result;
+
+    switch (operator) {
+
+        case '+':
+
+            result = operand1 + operand2;
+
+            printf("Result: %.2f\n", result);
+
+            break;
+
+        case '-':
+
+            result = operand1 - operand2;
+
+            printf("Result: %.2f\n", result);
+
+            break;
+
+        case '*':
+
+            result = operand1 * operand2;
+
+            printf("Result: %.2f\n", result);
+
+            break;
+
+        case '/':
+
+            if (operand2 != 0) {
+
+                result = operand1 / operand2;
+
+                printf("Result: %.2f\n", result);
+
+            } else {
+
+                printf("Error: Division by zero\n");
+
+            }
+
+            break;
+
+        default:
+
+            printf("Error: Invalid operator\n");
+
+            break;
+
+    }
+
+    return 0;
+
+}
