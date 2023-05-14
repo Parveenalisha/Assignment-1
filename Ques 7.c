2ba4d58f7423bda1987424f0adb15e3fd9979718
Parveenalisha <133557716+Parveenalisha@users.noreply.github.com>
2023-05-14 11:12:34 +0530

    Create Ques 7.c

---
+++ Ques 7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(
+int n1,n2,n3;
+printf("enter first number:");
+scanf("%d",&n1);
+printf("enter second number:");
+scanf("%d",&n2);
+printf("enter third number:");
+scanf("%d",&n3
+if (n1>n2){
+if(n1>n3){
+printf("the greates number is number 1");
+}
+else{
+printf("the greates number is number 3");
+}}
+else{
+if(n2>n3){
+printf("the greates number is number 2");
+}
+else{
+printf("the greates number is number 3");
+}}
+return 0;
+}
